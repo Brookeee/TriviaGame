e900d65363780e414228cd9d81913eb912f640d9
Brookee <oliver.brooke.a@gmail.com>
2019-05-25 21:01:44 -0400

    css/html/pseudo

---
+++ assets/javascript/app.js
@@ -0,0 +1,24 @@
+// Boy bands questions
+// 5 questions, 15 seconds (15000)
+
+// Questions object, answers array 
+
+// Q: What band sings 'I Want it That Way'?
+ // A: Backstreet Boys 
+ // Potential answers: 98 degrees, Boys II Men, New Kids on the Block 
+
+ // Q: Summer Girls was sang by?
+ // A: LFO
+ // Potential answers: B2K, BBMak, O-Town 
+
+ // Q: Justin Timberlake was part of what boy band? 
+ // A: N*SYNC
+ // Potential answers: 98 degrees, O-Town, Solo! He never was in a band 
+
+ // Q: Which name is not part of NKOTB? 
+ // A: Paul
+ // Potential answers: Donnie, Jordan, Joey
+
+// Q: Which band is the top selling group? 
+// A: Backstreet Boys 
+// Potential answers: Jackson 5, Hanson, The Beatles 
